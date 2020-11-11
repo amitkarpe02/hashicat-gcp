87033266ca8cf24efb4ce786119b8e3db981eb8e
Amit Karpe <akarpe@amfam.com>
2020-11-11 20:59:24 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+    source  = "terraform-google-modules/network/google//modules/vpc"
+    version = "~> 2.0.0"
+
+    project_id   = "var.project"
+    network_name = "ak-vpc"
+
+    shared_vpc_host = false
+
+}subnets = [
+  {
+    subnet_name   = "ak-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
