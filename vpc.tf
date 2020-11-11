73a62fdb61be1dc5038c0299dbcd2838cb200f20
Amit Karpe <akarpe@amfam.com>
2020-11-11 21:23:46 +0000

    Added new vpc2 module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,7 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google//modules/vpc"
     version = "~> 2.0.0"
-
     project_id   = "var.project"
     network_name = "ak1-vpc"
-
     shared_vpc_host = false
 }
